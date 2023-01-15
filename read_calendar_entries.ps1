96fcb8bdd3e5ea673d2bd4c95d97ceb9fad1708b
Robert Gaadt <122700218+rgaadt@users.noreply.github.com>
2023-01-15 14:31:18 +0100

    Add files via upload

---
+++ read_calendar_entries.ps1
@@ -0,0 +1,69 @@
+﻿#Declare Variables
+$EWSDLL = "C:\Program Files\Microsoft\Exchange Server\V15\Bin\Microsoft.Exchange.WebServices.dll"
+$MBX = "Mailbox_Name"
+$EWSURL = "https://yourExchangeDomainName/EWS/Exchange.asmx"
+$StartDate = (Get-Date).AddDays(-60)
+$EndDate = (Get-Date)  
+
+#Binding of the calendar of the Mailbox and EWS
+Import-Module -Name $EWSDLL
+$mailboxname = $MBX
+$service = new-object Microsoft.Exchange.WebServices.Data.ExchangeService([Microsoft.Exchange.WebServices.Data.Exchangeversion]::exchange2013)
+$service.Url = new-object System.Uri($EWSURL)
+$folderid= new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::Calendar,$MailboxName) 
+$Calendar = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)  
+$Recurring = new-object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition([Microsoft.Exchange.WebServices.Data.DefaultExtendedPropertySet]::Appointment, 0x8223,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::Boolean); 
+$psPropset= new-object Microsoft.Exchange.WebServices.Data.PropertySet([Microsoft.Exchange.WebServices.Data.BasePropertySet]::FirstClassProperties)  
+$psPropset.Add($Recurring)
+$psPropset.RequestedBodyType = [Microsoft.Exchange.WebServices.Data.BodyType]::Text;
+
+$RptCollection = @()
+
+$AppointmentState = @{0 = "None" ; 1 = "Meeting" ; 2 = "Received" ;4 = "Canceled" ; }
+
+#Define the calendar view  
+$CalendarView = New-Object Microsoft.Exchange.WebServices.Data.CalendarView($StartDate,$EndDate,1000)    
+$fiItems = $service.FindAppointments($Calendar.Id,$CalendarView)
+if($fiItems.Items.Count -gt 0){
+ $type = ("System.Collections.Generic.List"+'`'+"1") -as "Type"
+ $type = $type.MakeGenericType("Microsoft.Exchange.WebServices.Data.Item" -as "Type")
+ $ItemColl = [Activator]::CreateInstance($type)
+ foreach($Item in $fiItems.Items){
+  $ItemColl.Add($Item)
+ } 
+ [Void]$service.LoadPropertiesForItems($ItemColl,$psPropset)  
+}
+foreach($Item in $fiItems.Items){      
+ $rptObj = "" | Select StartTime,EndTime,Duration,Type,Subject,Location,Organizer,Attendees,AppointmentState,Notes,HasAttachments,IsReminderSet
+ $rptObj.StartTime = $Item.Start  
+ $rptObj.EndTime = $Item.End  
+ $rptObj.Duration = $Item.Duration
+ $rptObj.Subject  = $Item.Subject   
+ $rptObj.Type = $Item.AppointmentType
+ $rptObj.Location = $Item.Location
+ $rptObj.Organizer = $Item.Organizer.Address
+ $rptObj.HasAttachments = $Item.HasAttachments
+ $rptObj.IsReminderSet = $Item.IsReminderSet
+ $aptStat = "";
+ $AppointmentState.Keys | where { $_ -band $Item.AppointmentState } | foreach { $aptStat += $AppointmentState.Get_Item($_) + " "}
+ $rptObj.AppointmentState = $aptStat 
+ $RptCollection += $rptObj
+ foreach($attendee in $Item.RequiredAttendees){
+  $atn = $attendee.Address + "; "  
+  $rptObj.Attendees += $atn
+  }
+ foreach($attendee in $Item.OptionalAttendees){
+  $atn = $attendee.Address + "; "  
+  $rptObj.Attendees += $atn
+ }
+ foreach($attendee in $Item.Resources){
+  $atn = $attendee.Address + "; "  
+  $rptObj.Resources += $atn
+ }
+ $rptObj.Notes = $Item.Body.Text
+#Display on the screen
+ "Start:   " + $Item.Start  
+ "Subject: " + $Item.Subject 
+}   
+#Export to a CSVFile
+$RptCollection |  Export-Csv -NoTypeInformation -Path "c:\temp\$MailboxName-CalendarCSV.csv"
